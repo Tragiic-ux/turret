65bfacd533d8cc87ca28dc8df74836ff703f9ab1
Tragiic-ux <danaszaborovskis@gmail.com>
2026-03-14 13:09:56 +0000

    Update turret.ino

--- turret.ino
+++ turret.ino
@@ -1,37 +1,47 @@
-const int stepPin =9;
-const int dirPin =8;
-const int enPin =10;
+const int stepPin = 9;
+const int dirPin  = 11;
+const int enPin   = 10;
 
 void setup() {
-pinMode(stepPin, OUTPUT);
-pinMode(dirPin, OUTPUT);
-pinMode(enPin, OUTPUT);
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  pinMode(enPin, OUTPUT);
 
-digitalWrite(enPin, LOW);
-Serial.begin(9600);
-Serial.println("Stepper ready. Commands: +(CW), -(CCW), s (stop)");
-
+  digitalWrite(enPin, LOW); // enable driver
+  Serial.begin(9600);
+  Serial.println("Stepper ready. Commands: +(CW), -(CCW), s(stop), e(enable)");
 }
 
 void loop() {
-if (Serial.available()){
-  char cmd=Serial.read();
+  if (Serial.available()){
+    char cmd = Serial.read();
 
-  switch(cmd){
-    case"+":
-    digitalWrite(dirPin, HIGH); //clockwise
-    Serial.println("Clockwise");
-    break;
-    case "s":
-    digitalWrite(enPin, HIGH); //Disable motor
-    Serial.println("Stopped");
-    break;
-    case "e":
-    digitalWrite(enPin, LOW);
-    Serial.println("Enabled");
-    break;
+    switch(cmd){
+      case '+':
+        digitalWrite(dirPin, HIGH); // clockwise
+        Serial.println("Clockwise");
+        break;
 
+      case '-':
+        digitalWrite(dirPin, LOW);  // counter-clockwise
+        Serial.println("Counter-Clockwise");
+        break;
+
+      case 's':
+        digitalWrite(enPin, HIGH);  // disable
+        Serial.println("Stopped");
+        break;
+
+      case 'e':
+        digitalWrite(enPin, LOW);   // enable
+        Serial.println("Enabled");
+        break;
+    }
   }
-}
 
+  // step pulse
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(500); 
+  digitalWrite(stepPin, LOW);
+  delayMicroseconds(500);
 }
